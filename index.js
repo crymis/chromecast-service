f76cbdc8e56e97db2e0885fe2d8e4d7cf9978c74
Daniel Eckelt <deckelt@inovex.de>
2023-01-07 11:38:58 +0100

    add error handling for playing yt video

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 const port = 3333;
 
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const ytId = req.query.id;
   res.send(`Welcome to the DJonTour Library! Now Playing video with id: ${ytId || -''}`);
 
-  playYtVideo(ytId);
+  await playYtVideo(ytId).catch(err => {
+    console.error('Some error occured while trying to start playing a YouTube video. ', err);
+  });
+
   const nowTime = new Date().toLocaleTimeString();
   console.log(`${nowTime}: Now playing the YouTube video ${ytId}`);
 });
